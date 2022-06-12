414a5b66a75248d7189d863b927944571e262135
DavidHearl <david.hearl@outlook.com>
2022-06-12 22:59:27 +0100

    added sorting

--- products/views.py
+++ products/views.py
@@ -6,9 +6,46 @@ def all_products(request):
 	"""A view to show all products, inlcuding sorting and search queries"""
 
 	products = Product.objects.all()
+	query = None
+	categories = None
+	sort = None
+	direction = None
+
+	if request.GET:
+		if 'sort' in request.GET:
+			sortkey = request.GET['sort']
+			sort = sortkey
+			if sortkey == 'name':
+				sortkey = 'lower_name'
+				products = products.annotate(lower_name=Lower('name'))
+
+			if 'direction' in request:
+				direction = request.GET
+				if direction == 'desc':
+					sortkey = f'-{sortkey}'
+			products = products.order_by(sortkey)
+
+		if 'category' in request.GET:
+			categories = request.GET['category'].split(',')
+			products = products.filter(category__name__in=categories)
+			categories = Category.objects.filter(name__in=categories)
+
+		if 'q' in request.GET:
+			query = request.GET['q']
+			if not query:
+				message.error(request, "You didn't enter any search criteria!")
+				return redirect(reverse('products'))
+
+			queries = Q(name_icontains=query) | Q(description_icontains=query)
+			products = products.filter(queries)
+
+	current_sorting = f'{sort}_{direction}'
 
 	context = {
 		'products': products,
+		'search_term':query,
+		'current_categories': categories,
+		'current_sorting': current_sorting,
 	}
 
 	return render(request, 'products/products.html', context)
